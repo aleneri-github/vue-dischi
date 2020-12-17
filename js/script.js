9885dc653e9431f1f0dead9daf0879fc24e29610
Alessandro <aleneri242@gmail.com>
2020-12-17 16:09:26 +0100

    Creazione cartella

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var root = new Vue(
+  {
+    el: '#root',
+    data: {
+
+    },
+
+    mounted: function() {
+    for (var i = 1; i <= 10; i++) {
+      axios.get('')
+      .then((result) => {
+
+      });
+    }
+  }
+})
