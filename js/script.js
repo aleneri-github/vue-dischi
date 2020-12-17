ba8dc83cc68b9c884aef483f27ca32bc0f9290c2
Alessandro <aleneri242@gmail.com>
2020-12-17 16:41:15 +0100

    Aggiornamento

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+// author: "Bon Jovi"
+// genre: "Rock"
+// poster: "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg"
+// title: "New Jersey"
+// year: "1988"
+
 var root = new Vue(
   {
     el: '#root',
@@ -6,10 +12,12 @@ var root = new Vue(
     },
 
     mounted: function() {
+
     for (var i = 1; i <= 10; i++) {
       axios.get('https://flynn.boolean.careers/exercises/api/array/music')
       .then((result) => {
-        console.log(result);
+        let albums = result.data.response;
+        console.log(albums);
 
       });
     }
